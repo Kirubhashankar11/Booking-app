81e84bee62dbeb3c8b4dc91f5e1ac61aa88c6421
kirubhashankar11~ <kirubhashankar1108@gmail.com>
2024-03-15 18:43:22 +0530

    39:39

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,18 @@ import './App.css'
 import IndexPage from './pages/IndexPage'
 import { Routes, Route } from "react-router-dom";
 import LoginPage from './pages/LoginPage';
+import Layout from './Layout';
 
 
 function App() {
   return (
     <Routes>
-      <Route index element={<IndexPage />} />
-      <Route path='/login' element={<LoginPage/>} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Route>
     </Routes>
-    
+
   )
 }
 
