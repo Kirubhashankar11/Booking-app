0e1b3c0472cc2ec23bd2e2a90748559f86238cfd
kirubhashankar11~ <kirubhashankar1108@gmail.com>
2024-03-15 15:16:52 +0530

    resolved bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import IndexPage from './pages/IndexPage'
 import { Routes, Route } from "react-router-dom";
+import LoginPage from './pages/LoginPage';
 
 
 function App() {
   return (
     <Routes>
       <Route index element={<IndexPage />} />
+      <Route path='/login' element={<LoginPage/>} />
     </Routes>
     
   )
